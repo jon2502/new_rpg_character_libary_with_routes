3db683a8585ac76a2ac78d08d83f6e2cb74424d8
jon2502 <jonbjarke25@outlook.dk>
2024-02-23 20:33:09 +0100

    set up of front page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for='(character, i) in characters' :key='i'>
+      <!-- : placeres forand to så at vi kan binde data fra chracter til character detain parametern name -->
+      <router-link :to="{ name: 'characterDetail', params: {name: character.name, }}">
+          <h3>{{ character.name }}</h3>
+      </router-link>
   </div>
+  <section id="Libary">
+    <div class="infoContainer" v-for='(character, i) in characters' :key='i'>
+      <div>
+        <h2>{{ character.name }}</h2>
+        <ul>
+            <li>{{ character.race }}Race: Half Elf</li>
+            <li>{{ character.culture_background }}</li>
+            <li>{{ character.profession_class }}</li>
+            <li>{{ character.place_of_birth }}</li>
+        </ul>
+      </div>
+      <img :src="character.img" :alt="character.name">
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+      data() {
+          return {
+              //
+              characters: []
+          }
+      },
+      methods: {
+          async getcharacter() {
+              //fetch api
+              let characters = await fetch('http://localhost:3000');
+              //
+              let finalist = await characters.json();
+              console.log(finalist)
+              //
+              this.characters = finalist.character;
+              console.log(this.ch)
+          }
+      },
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+      created: function (){
+          //
+          this.getcharacter()
+      }
   }
-}
 </script>
+
+<style>
+    #Libary img{
+        width: 80%;
+    }
+    #Libary{
+        display: grid;
+        margin-top: 2vh;
+        grid-template-columns: repeat(2, 50%);
+        row-gap: 2vh;
+        padding-left: 5%;
+    }
+    .infoContainer{
+        display: grid;
+        grid-template-columns: repeat(2, 50%);
+    }
+    li{
+        list-style: none;
+    }
+</style>
---
+++ src/views/characterDetail.vue
@@ -0,0 +1,63 @@
+<template>
+     <section id="Grid">
+        <div>
+            <h1>{{ character.name }}</h1>
+            <p>{{ character.p1 }}</p>
+            <p>{{ character.p2 }}</p>
+            <p>{{ character.p3 }}</p>
+            <p>{{ character.p4 }}</p>
+            <p>{{ character.p5 }}</p>
+            <p>{{ character.p6 }}</p>
+            <p>{{ character.p7 }}</p>
+            <p>{{ character.p8 }}</p>
+            <p>{{ character.p9 }}</p>
+            <p>{{ character.p10 }}</p>
+            <p>{{ character.p11 }}</p>
+            <p>{{ character.p12 }}</p>
+        </div>
+        <img :src="character.img" :alt="character.name">
+    </section>
+    <aside>
+            side bar
+    </aside>
+</template>
+
+<script>
+export default {
+    //
+    props: ['name'],
+
+    data(){
+        return {
+            //
+            character: {}
+        }
+    },
+    methods: {
+        async getcharacters() {
+            // url
+            let url = `http://localhost:3000/characters/${this.name}`;
+            //fetch api with url
+            let character = await fetch(url);
+            // 
+            let finalist = await character.json();
+            //
+            this.character = finalist.character[0];
+        }
+    },
+    created: function (){
+        //
+        this.getcharacters()
+    }
+
+}
+</script>
+
+<style>
+#Grid{
+        display: grid;
+        padding-left: 4vw;
+        grid-template-columns: repeat(2, 50%);
+        place-items: center;
+    }
+</style>
