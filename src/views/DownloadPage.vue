29d80ac58f55646fe9500904e863857f19e6d990
jon2502 <jonbjarke25@outlook.dk>
2025-03-14 15:40:45 +0100

    added image thumbnail to pdf downloader

--- src/views/DownloadPage.vue
+++ src/views/DownloadPage.vue
@@ -2,13 +2,19 @@
     <h1>Downloads</h1>
     <p>Here you can find and download homebrewed material from classes to races.</p>
     <section v-for='(download, i) in downloads' :key='i'>
-        <button @click="downloadFile(download.link, download.name)">
-            Download {{ download.name }}
-        </button>
+        <div class="flex">
+            <!--run download funcion when a user click one of the buttons-->
+            <img :src="download.link.replace('pdf', 'png')" :alt="download.name" class="thumbnail">
+            <button @click="downloadFile(download.link, download.name)">
+                Download {{ download.name }}
+            </button>
+        </div>
     </section>
 </template>
 
 <script>
+
+//import and use SaveAS from file-saver package
 import { saveAs } from 'file-saver';
 
 export default {
@@ -21,7 +27,7 @@ export default {
         methods: {
             async getcharacter() {
 
-                //fetch api
+                //fetch api to get download info
                 let downloads = await fetch('http://localhost:3000/downloads');
                 //
                 let finalist = await downloads.json();
@@ -31,8 +37,11 @@ export default {
             },
             async downloadFile(link, name) {
                 try{
+                    //fetch link
                     let response = await fetch(link);
+                    //return response as a blob
                     let blob = await response.blob();
+                    //then save blob as filemane.pdf to get the file data as a pdf
                     saveAs(blob, name+".pdf");
                 }catch(error){
                     alert("oh no!");
@@ -48,4 +57,13 @@ export default {
 </script>
 
 <style>
+    .flex{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .thumbnail{
+        width: 7.5vw;
+        padding: 1.5vw;
+    }
 </style>
