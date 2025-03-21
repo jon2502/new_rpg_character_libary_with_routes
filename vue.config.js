3629d7fbee3cfd2b9ff97ef4b6ef86d326bfde61
jon2502 <jonbjarke25@outlook.dk>
2025-03-21 15:32:23 +0100

    updated for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/https://github.com/jon2502/new_rpg_character_libary_with_routes/' : '/'
 })
